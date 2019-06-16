c0bfb6b20c0bcdec1d791dec75157fa8364d5f98
Aaron-SCC <32554879+Aaron-SCC@users.noreply.github.com>
2019-06-15 19:44:15 -0700

    Adds custom title tags/makeLinks()

--- sp19/application/helpers/common_helper.php
+++ sp19/application/helpers/common_helper.php
@@ -112,3 +112,30 @@ if(!function_exists('bootswatchFeedback'))
 	} 
 
 }#end bootswatchFeedback()
+
+if(!function_exists('makeLinks'))
+{
+    function makeLinks($nav)
+    {
+        $myReturn = '';
+        foreach($nav as $key => $value)
+        {
+            $myReturn .= '<li><a href="' . $key . '">' . $value . '</a></li>' . PHP_EOL;
+        }
+        
+        return $myReturn;
+        
+//        var_dump($nav);
+//        die;
+
+//        return 'is this working?';
+    }
+}
+
+
+
+
+
+
+
+
